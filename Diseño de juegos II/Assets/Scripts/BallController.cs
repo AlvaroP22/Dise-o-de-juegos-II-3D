3f314e7ed37687afae194e81e86301f8cc5d95e1
AlvaroP22 <alvaro.pozos8662@alumnos.udg.mx>
2024-11-01 11:56:57 -0600

    4.1

---
+++ Diseño de juegos II/Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+        public Rigidbody rb;
+        public float impulseForce = 3f;
+
+        private bool ignoreNextcollision;
+
+        private void OncollisionEnter(Collision collision)
+        {
+            if(ignoreNextcollision)
+            {
+                return;
+            }
+
+            rb.velocity=Vector3.zero;
+            rb.AddForce(Vector3.up*impulseForce, ForceMode.Impulse);
+
+            ignoreNextcollision=true;
+            Invoke("AllownextCollision",0.2f);
+        }
+
+        private void AllownextCollision(){
+            ignoreNextcollision = false;
+        }
+
+}
